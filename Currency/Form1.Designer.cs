fdd108fd61337389ab4cd560b01306cadf1b2f2e
Ammarru <62930502+Ammarru@users.noreply.github.com>
2021-03-08 23:44:45 +0700

    finished

---
+++ Currency/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Currency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.btStart = new System.Windows.Forms.Button();
+			this.edPrice = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.edPrice)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.button2);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.btStart);
+			this.panel1.Controls.Add(this.edPrice);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(729, 89);
+			this.panel1.TabIndex = 0;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(393, 13);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(60, 13);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Dollars: 0 $";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(230, 13);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(82, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Wallet: 1000rub";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(642, 60);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "Sell";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(516, 60);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "Buy";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// btStart
+			// 
+			this.btStart.Location = new System.Drawing.Point(516, 9);
+			this.btStart.Name = "btStart";
+			this.btStart.Size = new System.Drawing.Size(201, 23);
+			this.btStart.TabIndex = 4;
+			this.btStart.Text = "Calculate next Day";
+			this.btStart.UseVisualStyleBackColor = true;
+			this.btStart.Click += new System.EventHandler(this.btStart_Click);
+			// 
+			// edPrice
+			// 
+			this.edPrice.DecimalPlaces = 2;
+			this.edPrice.Location = new System.Drawing.Point(89, 11);
+			this.edPrice.Name = "edPrice";
+			this.edPrice.Size = new System.Drawing.Size(104, 20);
+			this.edPrice.TabIndex = 1;
+			this.edPrice.Value = new decimal(new int[] {
+            7462,
+            0,
+            0,
+            131072});
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(23, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(60, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Initial price:";
+			// 
+			// chart1
+			// 
+			chartArea1.AxisX.Minimum = 0D;
+			chartArea1.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea1);
+			this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.chart1.Location = new System.Drawing.Point(0, 89);
+			this.chart1.Name = "chart1";
+			series1.BorderWidth = 3;
+			series1.ChartArea = "ChartArea1";
+			series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+			series1.IsValueShownAsLabel = true;
+			series1.LabelFormat = "f2";
+			series1.Name = "Series1";
+			this.chart1.Series.Add(series1);
+			this.chart1.Size = new System.Drawing.Size(729, 361);
+			this.chart1.TabIndex = 1;
+			this.chart1.Text = "chart1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(729, 450);
+			this.Controls.Add(this.chart1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.edPrice)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.NumericUpDown edPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Currency/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int days = 1;
+        const double k = 0.03;
+        bool clicked = false;
+		 double rub = 1000d;
+		double dollars = 0d;
+		double price;
+		private void btStart_Click(object sender, EventArgs e)
+        {
+
+			if (!clicked)
+			{
+				Random random = new Random();
+				price = (double)edPrice.Value;
+				chart1.Series[0].Points.AddXY(0, price);
+				days++;
+				price = price * (1 + k * (random.NextDouble() - 0.5));
+				chart1.Series[0].Points.AddXY(days, price);
+				clicked = true;
+			}
+			else
+			{
+
+				Random random = new Random();
+				price = (double)edPrice.Value;
+				days++;
+				price = price * (1 + k * (random.NextDouble() - 0.5));
+				chart1.Series[0].Points.AddXY(days, price);
+			}
+            
+        }
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (rub < price)
+			{
+				MessageBox.Show("Not EnoughMoney");
+			}
+			else
+			{
+				dollars++;
+				label4.Text = $"{dollars} $";
+				rub -= price;
+				label3.Text = $"{rub} rub";
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (dollars <= 0)
+			{
+				MessageBox.Show("Not Enough Dollars!");
+			}
+			else
+			{
+				dollars--;
+				label4.Text = $"{dollars} $";
+				rub += price;
+				label3.Text = $"{rub} rub";
+			}
+		}
+	}
+}
